eda03ebbfddfff9133c24625965061519a35516b
Akshai Binuraj <akshai.binuraj@chronus.com>
2023-06-06 15:14:11 +0530

    Article update and save added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,32 @@ class ArticlesController < ApplicationController
     def index
         @articles = Article.all
     end    
+
+    def new
+        @article = Article.new 
+    end
+    
+    def create
+        @article = Article.new(params.require(:article).permit(:title))
+        if @article.save
+          flash[:notice] = "Article displayed successfully"  
+          redirect_to @article
+        else
+            render 'new'
+        end    
+    end   
+    
+    def edit
+        @article = Article.find(params[:id])
+    end
+    
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title))
+            flash[:notice] = "Article updated successfully"
+            redirect_to @article
+        else
+            render 'edit'
+        end        
+    end    
 end        
