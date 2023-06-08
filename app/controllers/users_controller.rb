adefa2a15ed2dd1f616753a978f4de599fa8b43d
Akshai Binuraj <akshai.binuraj@chronus.com>
2023-06-08 16:47:13 +0530

    Edit users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,21 @@ class UsersController < ApplicationController
         else
             render 'new'
         end
-    end  
+    end 
+
+    def edit
+        @user = User.find(params[:id])
+    end    
+    
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            flash[:notice] = "Your account information was successfully updated"
+            redirect_to articles_path
+        else
+            render 'edit'
+        end       
+    end    
     
     private
     def user_params
