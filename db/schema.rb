99aca969ca2f04052662506d5c0608df8114cf4d
Akshai Binuraj <akshai.binuraj@chronus.com>
2023-06-08 12:26:08 +0530

    User created

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_05_102615) do
+ActiveRecord::Schema.define(version: 2023_06_08_063405) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
