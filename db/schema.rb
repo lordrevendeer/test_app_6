386ce2a11c1f6ca5ee1dca8af77a0f5d4c16d118
Akshai Binuraj <akshai.binuraj@chronus.com>
2023-06-09 17:05:45 +0530

    Admin table created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_08_090249) do
+ActiveRecord::Schema.define(version: 2023_06_09_112731) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2023_06_08_090249) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
