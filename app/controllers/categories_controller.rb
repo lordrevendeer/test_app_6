5c1e4b02d9d6c2e3e3894f04f1992b5e29d800e9
Akshai Binuraj <akshai.binuraj@chronus.com>
2023-06-13 12:40:40 +0530

    V2

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,5 @@
 class CategoriesController < ApplicationController
+   before_action :require_admin, except: [:index, :show]
    def new
      @category = Category.new
    end
@@ -26,4 +27,11 @@ class CategoriesController < ApplicationController
    def category_params
      params.require(:category).permit(:name)
    end  
+
+   def require_admin
+    if !(logged_in? && current_user.admin?)
+      flash[:alert] = "Action restricted to admins"
+      redirect_to categories_path
+    end  
+   end 
 end    
