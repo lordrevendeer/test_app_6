c9667b1f39bf50782788c07b70a2092de6b99822
Akshai Binuraj <akshai.binuraj@chronus.com>
2023-06-17 11:53:51 +0530

    V10

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,6 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("chartkick")
-require (Chart.bundle)
 require("chart.js")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
