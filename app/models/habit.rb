b27c58786e18becbbfa87b93b0ded432eba31ad3
Akshai Binuraj <akshai.binuraj@chronus.com>
2023-06-19 19:42:07 +0530

    V21

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -1,7 +1,8 @@
 class Habit < ApplicationRecord
     belongs_to :user
     belongs_to :category
-    validates :title, presence: TRUE, length: {minimum: 5,maximum: 50}
-    validates :description, presence: TRUE, length: {minimum: 8,maximum: 500}
+    validates :title, presence: TRUE, length: {minimum: 3,maximum: 50}
+    validates :description, presence: TRUE, length: {minimum: 3,maximum: 500}
     validates :days, numericality: { only_integer: true, greater_than: 0 }
+    validates_uniqueness_of :frequency, :scope => [:title, :description, :days], :message => ",description,days,title combination must be unique"
  end
